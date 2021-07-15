f63c82e5c0ae6d2cd8d93da896e480c4f32ec92d
devThinKoki <85640163+devThinKoki@users.noreply.github.com>
2021-07-15 11:54:24 +0900

    [M]_[Easy]Generating-Randomness_02

--- JetBrains_Academy/Python_Development_Course/Easy_Generating-Randomness/02_Analyzing-user-input/02_Analyzing-user-input.py
+++ JetBrains_Academy/Python_Development_Course/Easy_Generating-Randomness/02_Analyzing-user-input/02_Analyzing-user-input.py
@@ -8,13 +8,21 @@ while True:
         break
     print(f'Current data length is {len(data_list)}, {100 - len(data_list)} symbols left')
 print()
-print(f"Final data string:\n{''.join(data_list)}")
+final_str = ''.join(data_list)
+print(f"Final data string:\n{final_str}")
 
-# 000: 0,3
-# 001: 10,5
-# 010: 13,18
-# 011: 5,2
-# 100: 3,12
-# 101: 20,3
-# 110: 2,5
-# 111: 2,1
+bins = [str(i) + str(j) + str(k) for i in range(2) for j in range(2) for k in range(2)]
+for i in range(len(bins)):
+    cnt_0, cnt_1, idx = 0, 0, -1
+    while  idx < len(final_str) - 4:
+        temp_idx = final_str.find(bins[i], idx + 1)
+        if temp_idx == -1:
+            break
+        if temp_idx == len(final_str) - 3:
+            break
+        if final_str[temp_idx + 3] == '0':
+            cnt_0 += 1
+        else:
+            cnt_1 += 1
+        idx = temp_idx
+    print(f"{bins[i]}: {cnt_0},{cnt_1}")
